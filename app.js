f0904551fa35bd98af26287b5ac8367ee96da341
ria-nanuda <kbr@widaehan.com>
2022-01-13 15:41:04 +0900

    chaged port

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const app = express();
-const port = 3002;
+const port = 3001;
 app.set("port", port);
 app.get("/", (req, res) => {
   res.send("Hello amazon!");
